0c043bef64fc6c17512e36c6d12309ba695fdf4a
NelsonNV <nelsonluisnv2@gmail.com>
2025-01-07 22:50:32 -0300

    refactore: configurar supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,25 +1,49 @@
 return {
   "supermaven-inc/supermaven-nvim",
-  lazy = true,
+  dependencies = {
+    "onsails/lspkind.nvim",
+  },
   config = function()
+    -- Configuración de lspkind
+    require("lspkind").init()
+
+    -- Configuración de supermaven-nvim
     require("supermaven-nvim").setup({
       keymaps = {},
-      ignore_filetypes = { cpp = true }, -- or { "cpp", }
+      ignore_filetypes = { cpp = true },
       color = {
-        -- suggestion_color = "#EDE3E4",
-        suggestion_color = "#FF0000",
+        suggestion_color = "#EDE3E4",
         cterm = 244,
       },
-      log_level = "info", -- set to "off" to disable logging completely
-      disable_inline_completion = false, -- disables inline completion for use with cmp
-      disable_keymaps = false, -- disables built in keymaps for more manual control
+      log_level = "info",
+      disable_inline_completion = false,
+      disable_keymaps = false,
+      formatting = {
+        format = require("lspkind").cmp_format({
+          mode = "symbol",
+          max_width = 50,
+          symbol_map = { Supermaven = "" },
+        }),
+      },
       condition = function()
         return false
-      end, -- condition to check for stopping supermaven, `true` means to stop supermaven when the condition is true.
+      end,
     })
 
+    -- Verificación de funciones antes de asignar keymaps
+    local supermaven = require("supermaven-nvim")
     local completion_preview = require("supermaven-nvim.completion_preview")
-    vim.keymap.set("i", "<TAB>", completion_preview.on_accept_suggestion, { noremap = true, silent = true })
-    vim.keymap.set("i", "<c-j>", completion_preview.on_accept_suggestion_word, { noremap = true, silent = true })
+
+    if supermaven and completion_preview.on_accept_suggestion then
+      vim.keymap.set("i", "<c-y>", completion_preview.on_accept_suggestion, { noremap = true, silent = true })
+    else
+      vim.notify("supermaven-nvim: on_accept_suggestion no está definido", vim.log.levels.WARN)
+    end
+
+    if completion_preview and completion_preview.on_accept_suggestion_word then
+      vim.keymap.set("i", "<c-j>", completion_preview.on_accept_suggestion_word, { noremap = true, silent = true })
+    else
+      vim.notify("supermaven-nvim: on_accept_suggestion_word no está definido", vim.log.levels.WARN)
+    end
   end,
 }

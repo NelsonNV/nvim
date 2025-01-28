b8b76b35ce3cc8cf5ab70cc69f756a38f6d23a72
NelsonNV <nelsonluisnv2@gmail.com>
2025-01-28 12:38:19 -0300

    feat:color mas notorio en las ventanas separadas

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,5 +1,16 @@
 return {
-  "folke/tokyonight.nvim",
-  lazy = true,
-  opts = { style = "moon" },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = {
+      style = "moon",
+      on_highlights = function(hl, c)
+        hl.WinSeparator = {
+          fg = "#9d7cd8", -- color purple tokyonight
+          bg = "None",
+          bold = true,
+        }
+      end,
+    },
+  },
 }

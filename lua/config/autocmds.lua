348e066f7f0f06984336b2b7da79632720150c85
NelsonNV <nelsonluisnv2@gmail.com>
2025-02-01 14:19:17 -0300

    feat: color al spacio en blanco

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,16 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Highlight trailing whitespace
+
+local colorWhiteSpaceFont = "#ff007c"
+local colorWitheSpaceBackground = "#414868"
+
+vim.cmd(
+  string.format(
+    [[ autocmd BufReadPre * hi TrailingWhitespace guibg=%s guifg=%s | match TrailingWhitespace /\s\+$/ ]],
+    colorWitheSpaceBackground,
+    colorWhiteSpaceFont
+  )
+)

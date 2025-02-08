729307b7bc732f20cd7db67f1aa7c5f0d64a4b72
NelsonNV <nelsonluisnv2@gmail.com>
2025-02-08 19:29:56 -0300

    feat: transparencia

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,19 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-      style = "moon",
-      on_highlights = function(hl, c)
-        hl.WinSeparator = {
-          fg = "#9d7cd8", -- color purple tokyonight
-          bg = "None",
-          bold = true,
-        }
-      end,
+  "folke/tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = {
+    transparent = true,
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
     },
+    on_highlights = function(hl, c)
+      hl.WinSeparator = {
+        fg = "#9d7cd8", -- color purple tokyonight
+        bg = "None",
+        bold = true,
+      }
+    end,
   },
 }

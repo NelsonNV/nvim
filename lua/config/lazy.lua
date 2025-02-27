70dc8b640814e3be258f5e8bd888e3072c4baacc
NelsonNV <nelsonluisnv2@gmail.com>
2025-02-27 14:59:32 -0300

    refactor: carpeta ui

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "plugins" },
+    { import = "plugins.ui" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.

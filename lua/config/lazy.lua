3dbaaf5db2fcc258eacef71a5765b7e642436299
NelsonNV <nelsonluisnv2@gmail.com>
2025-05-09 12:15:22 -0400

    refactor: comment lazy extra plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,9 +12,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.php" },
-    { import = "lazyvim.plugins.extras.lang.docker" },
+    -- { import = "lazyvim.plugins.extras.lang.python" },
+    -- { import = "lazyvim.plugins.extras.lang.php" },
+    -- { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.formatting.biome" },
 

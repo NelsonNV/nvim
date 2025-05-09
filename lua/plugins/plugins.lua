beae9b17751dff62f71f4bfa43726e7243a69e33
NelsonNV <nelsonluisnv2@gmail.com>
2025-05-09 12:13:38 -0400

    feat: ajustes  lsp para php

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,1 +1,22 @@
-return {}
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        intelephense = {
+          settings = {
+            intelephense = {
+              environment = {
+                includePaths = { "vendor" }, -- Ruta al directorio vendor
+              },
+              -- Opcional: Deshabilita advertencias si usas Eloquent sin Laravel
+              diagnostics = {
+                undefinedTypes = false,
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}

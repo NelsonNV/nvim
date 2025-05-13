51c5ab1b5c84c7c6f9604b5d45c8f00762f51289
NelsonNV <nelsonluisnv2@gmail.com>
2025-05-12 21:06:17 -0400

    chore: comment keymap

--- lua/plugins/utils/emojis.lua
+++ lua/plugins/utils/emojis.lua
@@ -5,7 +5,7 @@ return {
 
     local opts = { noremap = true, silent = true }
 
-    vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
+    -- vim.keymap.set("n", "<Leader><Leader>i", "<cmd>IconPickerNormal<cr>", opts)
     vim.keymap.set("i", "<C-i>", "<cmd>IconPickerInsert<cr>", opts)
   end,
 }

17aea7e0ecfd5e020a6a1b6f4c2add4c3e4e9890
NelsonNV <nelsonluisnv2@gmail.com>
2025-06-11 20:15:57 -0400

    feat: explore mostrar en la derecha

--- lua/plugins/core/snacks.nvim.lua
+++ lua/plugins/core/snacks.nvim.lua
@@ -1,4 +1,13 @@
 return {
   "folke/snacks.nvim",
-  opts = { dashboard = { enabled = false } },
+  opts = {
+    dashboard = { enabled = false },
+    picker = {
+      sources = {
+        explorer = {
+          layout = { layout = { position = "right" } },
+        },
+      },
+    },
+  },
 }

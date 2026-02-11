19bf2140247f581344790c8cdf2a66d6c7f0aaa5
NelsonNV <nelsonluisnv2@gmail.com>
2026-02-11 20:44:09 -0300

    revert: vuelvo a usar tokyonight

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -1,9 +1,19 @@
 return {
-	"RedsXDD/neopywal.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		-- Selecciona automáticamente el esquema según el fondo
-		vim.cmd.colorscheme("neopywal-dark")
-	end,
+  "folke/tokyonight.nvim",
+  lazy = true,
+  priority = 1000,
+  opts = {
+    -- transparent = true,
+    -- styles = {
+    --   sidebars = "transparent",
+    --   floats = "transparent",
+    -- },
+    -- on_highlights = function(hl, c)
+    --   hl.WinSeparator = {
+    --     fg = "#9d7cd8", -- color purple tokyonight
+    --     bg = "None",
+    --     bold = true,
+    --   }
+    -- end,
+  },
 }
